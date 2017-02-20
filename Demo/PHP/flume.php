3dae5ec1ddfe276f6e13a9d46b866ed5739624b4
jirenyou <390066398@qq.com>
2017-02-21 01:49:16 +0800

    修改flume案例

--- Demo/PHP/flume.php
+++ Demo/PHP/flume.php
@@ -5,7 +5,8 @@ try{
     $flume = Flume::getIns('192.168.1.180', 8888, 'tcp', 30, STREAM_CLIENT_ASYNC_CONNECT);
     
     //发送数据到 Flume-ng
-    $data = '{name:"jirneyou",nick:"24K村帅-2113"}';
+    @$msg = $_GET['data'] ? $_GET['data'] : "test";
+    $data = '{data:"' . $msg . '"}';
     $rs = $flume::push($data);
     
     var_dump($rs);
